2377a28c6c9ca14585d054da7cd825282b81f193
felipebat20 <felipebatista24680@gmail.com>
2023-03-23 21:51:05 -0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header
+      class="
+        sticky
+        top-0
+        z-50
+        flex
+        justify-between
+        items-center
+        space-x-1
+        border-b
+        bg-white
+        p-4
+        shadow-md
+      "
+    >
+      <a class="text-3xl font-mono" href="#">cartrader</a>
+    </header>
+
+    <div
+      class="
+        relative
+        h-screen
+        w-100
+        bg-no-repeat
+        bg-cover
+        bg-bottom
+        bg-[url('https://cdn.ferrari.com/cms/network/media/img/resize/5f5b66b7b6dc0602f4e66bb6-ferrari-portofino-m-intro_desktop?width=1920&height=1600')]
+      "
+    >
+teste
+    </div>
+  </div>
+</template>
+
+123
