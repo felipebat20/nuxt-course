fb0e8d79c1c4a4e5c7d3a9767d2bd8153f0cda56
felipebat20 <felipebatista24680@gmail.com>
2023-03-25 12:38:28 -0300

    feat: car card

--- app.vue
+++ app.vue
@@ -64,6 +64,34 @@
           </h3>
         </div>
       </div>
+
+      <div class="w-full">
+        <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+          <div class="flex h-full">
+            <img
+              src="https://imgd.aeplcdn.com/0x0/n/cw/ec/106785/exterior-right-front-three-quarter-2.jpeg?isig=0"
+              alt=""
+              class="w-[300px] h-full"
+            />
+
+            <div class="p-4 flex flex-col">
+              <div>
+                <h1 class="text-2xl text-blue">
+                  Volvo
+                </h1>
+
+                <p class="text-gray-700">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+
+                <h1 class="mt-auto text-xl">
+                  $39,555
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
