6c9477bd377c1928e6b0f5b1f624bcdd0fd5a603
felipebat20 <felipebatista24680@gmail.com>
2023-03-25 08:47:28 -0300

    feat: search bar

--- app.vue
+++ app.vue
@@ -1,48 +1,5 @@
 <template>
-  <div>
-    <header
-      class="
-        sticky
-        top-0
-        z-50
-        flex
-        justify-between
-        items-center
-        space-x-1
-        border-b
-        bg-white
-        p-4
-        shadow-md
-      "
-    >
-      <a class="text-3xl font-mono" href="#">cartrader</a>
-    </header>
 
-    <div
-      class="
-        relative
-        h-screen
-        w-100
-        bg-no-repeat
-        bg-cover
-        bg-bottom
-        bg-[url('https://i.pinimg.com/originals/44/13/db/4413db979f336606a3a90926669a6c6e.jpg')]
-      "
-    >
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-10 text-white">
-            Find your drive
-          </h1>
-        </div>
-      </div>
-
-      <div class="z-1 absolute w-full h-full bg-black opacity-50">
-
-      </div>
-    </div>
-
-  </div>
 </template>
 
 123
