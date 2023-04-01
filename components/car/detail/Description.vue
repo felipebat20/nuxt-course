b361b8037656d90a47c3eba4eedd62269e095d3f
felipebat20 <felipebatista24680@gmail.com>
2023-04-01 15:06:30 -0300

    feat: unique dynamic paths

---
+++ components/car/detail/Description.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque accusantium vitae incidunt dolorem
+      voluptatibus dignissimos repellat natus cumque qui sunt quisquam dolores aliquam a praesentium, quis
+      laudantium fugit, fugiat sapiente.
+    </p>
+  </div>
+</template>
---
+++ components/car/detail/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mt-10">
+    <img src="https://imgd.aeplcdn.com/0x0/n/cw/ec/106785/exterior-right-front-three-quarter-2.jpeg?isig=0" alt=""
+      class="w-full" />
+
+    <h1 class="mt-10 text-4xl">
+      Volvo 3XT
+    </h1>
+
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">
+          5 seats
+        </p>
+
+        <p class="mr-2">
+          |
+        </p>
+
+        <p class="mr-2">
+          14,666 miles
+        </p>
+      </div>
+
+      <div>
+        <p class="font-bold text-2xl">
+          $25,555
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
