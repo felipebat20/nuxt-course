0fe6689fc835e3e63b776963f18185ba21b32852
felipebat20 <felipebatista24680@gmail.com>
2023-04-01 16:47:39 -0300

    feat: define custom layout

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-    <div>
-      <CarDetailHero />
-      <CarDetailAttributes />
-      <CarDetailDescription />
-      <CarDetailContact />
-    </div>
+  <div>
+    <CarDetailHero />
+    <CarDetailAttributes />
+    <CarDetailDescription />
+    <CarDetailContact />
   </div>
 </template>
 
@@ -14,6 +12,10 @@
 
   useHead({
     title: toTitleCase(route.params.name),
+  });
+
+  definePageMeta({
+    layout: 'custom',
   });
 
   function toTitleCase(str) {
