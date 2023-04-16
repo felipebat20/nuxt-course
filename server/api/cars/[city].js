d664fe3d689bdac226f7cd838f785e22fa85ecdb
felipebat20 <felipebatista24680@gmail.com>
2023-04-16 18:42:50 -0300

    feat: create api filter json cars

---
+++ server/api/cars/[city].js
@@ -0,0 +1,11 @@
+import cars from '@/data/cars.json'
+
+export default defineEventHandler((event) => {
+  const { city } = event.context.params;
+
+  let filteredCars = cars;
+
+  filteredCars = filteredCars.filter(car => car.city.toLowerCase() === city.toLowerCase())
+
+  return filteredCars;
+});
