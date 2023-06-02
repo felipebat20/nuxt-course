34feafae9309aa36ec013fbf1596599c90f06939
felipebat20 <felipebatista24680@gmail.com>
2023-06-02 20:09:13 -0300

    corrige login

--- pages/login.vue
+++ pages/login.vue
@@ -17,9 +17,24 @@
 
   const supabase = useSupabaseClient();
 
+  const getUrl = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ??
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+      'http://localhost:3000/';
+
+    url = url.includes('http') ? url : `https://${url}`;
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+
+    return url;
+  }
+
   const login = async () => {
     const { error } = supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: getUrl(),
+      }
     });
   }
 </script>
